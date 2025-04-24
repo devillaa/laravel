7d18d02636fab16d40697d22457ec5e18754ca39
devillaa <devillajoaopedro532@gmail.com>
2025-04-24 11:29:51 -0300

    keepinho aula 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CalculosController;
+use App\Http\Controllers\KeepinhoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,9 +16,14 @@ Route::get('/teste/{valor}', function ($valor) {
     return "Você digitou: {$valor}";
 });
 
-Route::get('/soma/{valor1}/{valor2}', function ($valor1, $valor2) {
-    $resultado = $valor1 + $valor2;
-    return 'A soma dos dois valores é: '. $resultado;
-});
-
 //Calculos
+Route::get('/calc/somar/{x}/{y}', [CalculosController::class, 'somar']);
+
+Route::get('/calc/subtrair/{x}/{y}', [CalculosController::class, 'subtrair']);
+
+Route::get('/calc/quadrado/{x}', [CalculosController::class, 'quadrado']);
+
+// Keepinho 
+Route::prefix('/keep') ->group(function(){
+    Route::get('/',[KeepinhoController::class, 'index']);
+});
