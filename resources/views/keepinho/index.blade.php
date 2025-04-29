36972462bebe5c147a81721b69e1f85c1d3ed469
devillaa <devillajoaopedro532@gmail.com>
2025-04-29 08:42:55 -0300

    Rota nota
    Form de nota, ainda n adiciona no banco

--- resources/views/keepinho/index.blade.php
+++ resources/views/keepinho/index.blade.php
@@ -1,2 +1,16 @@
 <h1>💡Keepinho</h1>
 <p>Seja bem-vindo ao Keepinho, o seu assistente pessoal (melhor do que o google).</p>
+<hr>
+
+<form action="{{ route('keep.gravar') }}" method="post">
+    @csrf
+    <textarea name="texto" cols="30" rows="10"></textarea>
+    <br> <br>
+    <input type="submit" value="Gravar Nota">
+</form>
+
+@foreach ($notas as $nota )
+    <div style="border:1px dashed green; margin-bottom:10px">
+        {{ $nota->texto }}
+    </div>
+@endforeach
